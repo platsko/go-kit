b1d9c2ec32b76f368b39c0c07d2f70892c274010
Petr Latsko <p.latsko@evenlab.org>
2021-03-12 15:19:41 +0300

    KIT-2 Bytes equal and rand functions

---
+++ bytes/bytes_test.go
@@ -0,0 +1,84 @@
+// Copyright © 2020-2021 The EVEN Solutions Developers Team
+
+package bytes_test
+
+import (
+	"testing"
+
+	. "github.com/platsko/go-kit/bytes"
+)
+
+func Benchmark_Equal(tb *testing.B) {
+	const size = 1024
+	vx, vy := make([]byte, size), RandBytes(size)
+	copy(vx, vy)
+	tb.ResetTimer()
+	for i := 0; i < tb.N; i++ {
+		Equal(vx, vy)
+	}
+}
+
+func Test_Equal(t *testing.T) {
+	t.Parallel()
+
+	const size = 1024
+	vx, vy := make([]byte, size), RandBytes(size)
+	copy(vx, vy)
+
+	tests := [7]struct {
+		name string
+		vx   []byte
+		vy   []byte
+		want bool
+	}{
+		{
+			name: "TRUE",
+			vx:   vx,
+			vy:   vy,
+			want: true,
+		},
+		{
+			name: "nil_VX_&_VY_TRUE",
+			vx:   nil,
+			vy:   nil,
+			want: true,
+		},
+		{
+			name: "zeros_VX_&_VY_TRUE",
+			vx:   []byte{},
+			vy:   []byte{},
+			want: true,
+		},
+		{
+			name: "FALSE",
+			vx:   vx,
+			vy:   RandBytes(size),
+		},
+		{
+			name: "nil_VX_FALSE",
+			vx:   nil,
+			vy:   vy,
+		},
+		{
+			name: "nil_VY_FALSE",
+			vx:   vx,
+			vy:   nil,
+		},
+		{
+			name: "len_diff_FALSE",
+			vx:   []byte{},
+			vy:   []byte{255},
+		},
+	}
+
+	for idx := range tests {
+		test := tests[idx]
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := Equal(test.vx, test.vy); got != test.want {
+				t.Errorf("Equal() got: %v | want: %v", got, test.want)
+			}
+		})
+	}
+}
---
+++ bytes/rand_test.go
@@ -0,0 +1,42 @@
+// Copyright © 2020-2021 The EVEN Solutions Developers Team
+
+package bytes_test
+
+import (
+	"testing"
+
+	. "github.com/platsko/go-kit/bytes"
+)
+
+func Benchmark_RandBytes(tb *testing.B) {
+	const size = 1024
+	tb.ResetTimer()
+	for i := 0; i < tb.N; i++ {
+		_ = RandBytes(size)
+	}
+}
+
+func TestRandBytes(t *testing.T) {
+	t.Parallel()
+
+	tests := [1]struct {
+		name string
+		size int
+	}{
+		{
+			name: "OK",
+			size: 1024,
+		},
+	}
+
+	for idx := range tests {
+		test := tests[idx]
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := RandBytes(test.size); len(got) != test.size {
+				t.Errorf("RandBytes() size: %v | want: %v", got, test.size)
+			}
+		})
+	}
+}
