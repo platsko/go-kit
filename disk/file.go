05d36c62f9d1829be1e87eb52231cd9e4994b9cd
Petr Latsko <p.latsko@evenlab.org>
2021-03-12 16:46:31 +0300

    KIT-7 Helpful disk functions - Make dirs - Make file

---
+++ disk/file.go
@@ -0,0 +1,20 @@
+// Copyright © 2020-2021 The EVEN Solutions Developers Team
+
+package disk
+
+import (
+	"os"
+)
+
+const (
+	// DefaultFilePerm contents the default Unix permission bits for file.
+	DefaultFilePerm = 0o644
+
+	// DefaultFileMode controls the default permissions on any file.
+	DefaultFileMode = os.FileMode(DefaultFilePerm)
+)
+
+// MakeFile creates or truncates the file.
+func MakeFile(name string) (*os.File, error) {
+	return os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, DefaultFileMode)
+}
