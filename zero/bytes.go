241818d9884f6b844d6f8342ef0cb353964d884f
Petr Latsko <p.latsko@evenlab.org>
2021-03-19 14:53:59 +0300

    KIT-22 improvements zero-bytes functions - append Bytea28 zeroing - append missed unit & benchmark tests (100% coverage) - code clean, refactoring and minor fixes

--- zero/bytes.go
+++ zero/bytes.go
@@ -2,22 +2,28 @@
 
 package zero
 
+// Bytea28 clears the 28-byte (224 bits) array by filling it with the zero value.
+// This is used to explicitly clear private key material from memory.
+func Bytea28(b *[28]byte) {
+	*b = [28]byte{}
+}
+
+// Bytea32 clears the 32-byte (256 bits) array by filling it with the zero value.
+// This is used to explicitly clear private key material from memory.
+func Bytea32(b *[32]byte) {
+	*b = [32]byte{}
+}
+
+// Bytea64 clears the 64-byte (512 bits) array by filling it with the zero value.
+// This is used to explicitly clear sensitive material from memory.
+func Bytea64(b *[64]byte) {
+	*b = [64]byte{}
+}
+
 // Bytes sets all bytes in the passed slice to zero.
 // This is used to explicitly clear data from memory.
 func Bytes(b []byte) {
 	for idx := range b {
 		b[idx] = 0
 	}
-}
-
-// Bytea32 clears the 32-byte array by filling it with the zero value.
-// This is used to explicitly clear private key material from memory.
-func Bytea32(b *[32]byte) {
-	*b = [32]byte{}
-}
-
-// Bytea64 clears the 64-byte array by filling it with the zero value.
-// This is used to explicitly clear sensitive material from memory.
-func Bytea64(b *[64]byte) {
-	*b = [64]byte{}
 }
